b1bb42db32bbb69d9e670a7548fe7b8df4cb6f07
quemingyi.wudong <quemingyi.wudong@bytedance.com>
2025-05-23 00:15:34 +0800

    hotfix: update sdk version

--- Web/src/appConfig.ts
+++ Web/src/appConfig.ts
@@ -1,3 +1,6 @@
+/**
+ * @note You can use **https://demo.byteplus.com/rtc/demo/rtc_demo_special** as the test domain name of the server. However, this domain name only provides testing running and cannot be used for official operations.
+ */
 export const HOST = 'https://demo.byteplus.com/rtc/demo/rtc_demo_special';
 
 export const appId = 'Your_AppID';
